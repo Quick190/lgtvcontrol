7b350796d96bbab6e42db463184a98997a9114c0
pardini <pardini@6cdb0af4-e54e-0410-915f-319f60a19af1>
2008-06-05 08:06:26 +0000

    Hacks since LG sends different codes when reading current input than it accepts when setting.

--- LGSerialControlApp/Form1.cs
+++ LGSerialControlApp/Form1.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
-namespace LGSerialControlApp
-{
-    public partial class Form1 : Form
-    {
+namespace LGSerialControlApp {
+    public partial class Form1 : Form {
         private LGTVControl control;
+        private bool readingValues = true;
 
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
             control = new LGTVControl();
             control.readAllCurrentValues();
 
@@ -37,27 +28,31 @@ namespace LGSerialControlApp
                     rdMainAV1.Checked = true;
                     break;
             }
+
+            readingValues = false;
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
+        private void trackBar1_Scroll(object sender, EventArgs e) {
+            if (readingValues) return;
             chkMute.Checked = false;
             control.SetaVolume(trackBar1.Value);
         }
 
-        private void mainInputRadioChanged(object sender, EventArgs e)
-        {
+        private void mainInputRadioChanged(object sender, EventArgs e) {
+            if (readingValues) return;
             if (rdMainTV.Checked) {
                 control.setMainInputSource(LGTVInput.TVCable);
-            } else if (rdMainAV1.Checked) {
+            }
+            else if (rdMainAV1.Checked) {
                 control.setMainInputSource(LGTVInput.AV1);
-            } else if (rdMainHDMI1.Checked) {
+            }
+            else if (rdMainHDMI1.Checked) {
                 control.setMainInputSource(LGTVInput.HDMIDVI1);
             }
         }
 
-        private void chkMute_CheckedChanged(object sender, EventArgs e)
-        {
+        private void chkMute_CheckedChanged(object sender, EventArgs e) {
+            if (readingValues) return;
             control.setVolumeMuted(chkMute.Checked);
         }
     }
