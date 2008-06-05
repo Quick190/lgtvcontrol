1b515a0f92c58890478ec5225b3b13450872bffa
pardini <pardini@6cdb0af4-e54e-0410-915f-319f60a19af1>
2008-06-05 07:58:55 +0000

    Inconsistent, horrible code, but working.

--- LGSerialControlApp/Form1.Designer.cs
+++ LGSerialControlApp/Form1.Designer.cs
@@ -28,30 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdMainHDMI1 = new System.Windows.Forms.RadioButton();
+            this.rdMainAV1 = new System.Windows.Forms.RadioButton();
             this.rdMainTV = new System.Windows.Forms.RadioButton();
-            this.tdMainAV1 = new System.Windows.Forms.RadioButton();
-            this.tdMainHDMI1 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.chkMute = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar1.Location = new System.Drawing.Point(19, 193);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(295, 35);
-            this.trackBar1.TabIndex = 0;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.tdMainHDMI1);
-            this.groupBox1.Controls.Add(this.tdMainAV1);
+            this.groupBox1.Controls.Add(this.rdMainHDMI1);
+            this.groupBox1.Controls.Add(this.rdMainAV1);
             this.groupBox1.Controls.Add(this.rdMainTV);
             this.groupBox1.Location = new System.Drawing.Point(217, 12);
             this.groupBox1.Name = "groupBox1";
@@ -59,6 +49,30 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Input Principal";
+            // 
+            // rdMainHDMI1
+            // 
+            this.rdMainHDMI1.AutoSize = true;
+            this.rdMainHDMI1.Location = new System.Drawing.Point(6, 53);
+            this.rdMainHDMI1.Name = "rdMainHDMI1";
+            this.rdMainHDMI1.Size = new System.Drawing.Size(76, 17);
+            this.rdMainHDMI1.TabIndex = 2;
+            this.rdMainHDMI1.TabStop = true;
+            this.rdMainHDMI1.Text = "HDMI/DVI";
+            this.rdMainHDMI1.UseVisualStyleBackColor = true;
+            this.rdMainHDMI1.CheckedChanged += new System.EventHandler(this.mainInputRadioChanged);
+            // 
+            // rdMainAV1
+            // 
+            this.rdMainAV1.AutoSize = true;
+            this.rdMainAV1.Location = new System.Drawing.Point(6, 36);
+            this.rdMainAV1.Name = "rdMainAV1";
+            this.rdMainAV1.Size = new System.Drawing.Size(48, 17);
+            this.rdMainAV1.TabIndex = 1;
+            this.rdMainAV1.TabStop = true;
+            this.rdMainAV1.Text = "AV 1";
+            this.rdMainAV1.UseVisualStyleBackColor = true;
+            this.rdMainAV1.CheckedChanged += new System.EventHandler(this.mainInputRadioChanged);
             // 
             // rdMainTV
             // 
@@ -70,42 +84,44 @@
             this.rdMainTV.TabStop = true;
             this.rdMainTV.Text = "Analog TV";
             this.rdMainTV.UseVisualStyleBackColor = true;
+            this.rdMainTV.CheckedChanged += new System.EventHandler(this.mainInputRadioChanged);
             // 
-            // tdMainAV1
+            // trackBar1
             // 
-            this.tdMainAV1.AutoSize = true;
-            this.tdMainAV1.Location = new System.Drawing.Point(6, 36);
-            this.tdMainAV1.Name = "tdMainAV1";
-            this.tdMainAV1.Size = new System.Drawing.Size(48, 17);
-            this.tdMainAV1.TabIndex = 1;
-            this.tdMainAV1.TabStop = true;
-            this.tdMainAV1.Text = "AV 1";
-            this.tdMainAV1.UseVisualStyleBackColor = true;
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(12, 194);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(268, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
-            // tdMainHDMI1
+            // chkMute
             // 
-            this.tdMainHDMI1.AutoSize = true;
-            this.tdMainHDMI1.Location = new System.Drawing.Point(6, 53);
-            this.tdMainHDMI1.Name = "tdMainHDMI1";
-            this.tdMainHDMI1.Size = new System.Drawing.Size(76, 17);
-            this.tdMainHDMI1.TabIndex = 2;
-            this.tdMainHDMI1.TabStop = true;
-            this.tdMainHDMI1.Text = "HDMI/DVI";
-            this.tdMainHDMI1.UseVisualStyleBackColor = true;
+            this.chkMute.AutoSize = true;
+            this.chkMute.Location = new System.Drawing.Point(280, 208);
+            this.chkMute.Name = "chkMute";
+            this.chkMute.Size = new System.Drawing.Size(50, 17);
+            this.chkMute.TabIndex = 2;
+            this.chkMute.Text = "Mute";
+            this.chkMute.UseVisualStyleBackColor = true;
+            this.chkMute.CheckedChanged += new System.EventHandler(this.chkMute_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(335, 246);
+            this.Controls.Add(this.chkMute);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.trackBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +129,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton tdMainHDMI1;
-        private System.Windows.Forms.RadioButton tdMainAV1;
+        private System.Windows.Forms.RadioButton rdMainHDMI1;
+        private System.Windows.Forms.RadioButton rdMainAV1;
         private System.Windows.Forms.RadioButton rdMainTV;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox chkMute;
     }
 }
 
